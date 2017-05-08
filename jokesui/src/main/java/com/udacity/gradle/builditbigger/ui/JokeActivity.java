1e3e987fb560586f6b317280ee9042618245a544
Borja Quevedo <borjota@gmail.com>
2017-05-08 23:31:29 +0200

    Factory method to create a JokeActivity and look for the JOKE extra in the calling intent

--- jokesui/src/main/java/com/udacity/gradle/builditbigger/ui/JokeActivity.java
+++ jokesui/src/main/java/com/udacity/gradle/builditbigger/ui/JokeActivity.java
@@ -1,13 +1,30 @@
 package com.udacity.gradle.builditbigger.ui;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
+
+    private static final String JOKE_KEY = JokeActivity.class.getCanonicalName() + ".JOKE_KEY";
+
+    public static Intent createCallingIntent(Context context, String joke) {
+        Intent callingIntent = new Intent(context, JokeActivity.class);
+        callingIntent.putExtra(JOKE_KEY, joke);
+        return callingIntent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
+
+        Intent callingIntent = getIntent();
+        if (callingIntent.hasExtra(JOKE_KEY)) {
+            TextView jokeTextView = (TextView) findViewById(R.id.joke_textview);
+            jokeTextView.setText(callingIntent.getStringExtra(JOKE_KEY));
+        }
     }
 }
