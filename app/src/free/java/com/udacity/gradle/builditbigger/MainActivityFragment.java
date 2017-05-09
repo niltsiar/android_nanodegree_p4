38c80244eb04b7299fb333cdcbf095f691d5acc9
Borja Quevedo <borjota@gmail.com>
2017-05-09 23:46:22 +0200

    Show interstitial ad

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -5,13 +5,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends BaseActivityFragment {
+
+    private InterstitialAd interstitialAd;
 
     public MainActivityFragment() {
     }
@@ -24,6 +28,27 @@ public class MainActivityFragment extends BaseActivityFragment {
 
         adView.loadAd(createAdRequest());
 
+        interstitialAd = new InterstitialAd(getContext());
+        interstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
+
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                retrieveJoke();
+            }
+        });
+
+        requestNewInterstitial();
+
+        jokeButton.setOnClickListener(view -> {
+            if (interstitialAd.isLoaded()) {
+                interstitialAd.show();
+            } else {
+                retrieveJoke();
+            }
+        });
+
         return root;
     }
 
@@ -33,5 +58,9 @@ public class MainActivityFragment extends BaseActivityFragment {
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
         return new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                                       .build();
+    }
+
+    private void requestNewInterstitial() {
+        interstitialAd.loadAd(createAdRequest());
     }
 }
