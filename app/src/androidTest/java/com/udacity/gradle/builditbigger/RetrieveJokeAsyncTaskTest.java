4b1d6648085eb9312627cd1605dc7530f75ae3ef
Borja Quevedo <borjota@gmail.com>
2017-05-09 12:52:38 +0200

    Test for AsyncTask

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/RetrieveJokeAsyncTaskTest.java
@@ -0,0 +1,30 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import java.util.concurrent.TimeUnit;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(AndroidJUnit4.class)
+public class RetrieveJokeAsyncTaskTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testAsyncTask() throws
+                                Exception {
+        RetrieveJokeAsyncTask retrieveJokeAsyncTask = new RetrieveJokeAsyncTask(rule.getActivity());
+        retrieveJokeAsyncTask.execute();
+
+        String joke = retrieveJokeAsyncTask.get(45, TimeUnit.SECONDS);
+
+        assertNotNull(joke);
+        assertFalse(joke.isEmpty());
+    }
+}
